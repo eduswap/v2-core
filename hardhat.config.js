c8dec4d9cf5ac92d53681c31f768f79f853a543f
solidity12 <volibear1001@gmail.com>
2024-07-24 16:10:59 +0900

    feat: Update rpc

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,25 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.5.16",
+  solidity: {
+    compilers: [{
+      version: "0.5.16",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        }
+      },
+    }],
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://open-campus-codex-sepolia.drpc.org",
+      }
+    },
+    edutest: {
+      url: "https://open-campus-codex-sepolia.drpc.org",
+    },
+  }
 };
